31cff9b823337436a033d8d4fd4bb5da82266105
21CE077 <123922276+21CE077@users.noreply.github.com>
2023-07-26 10:50:48 +0530

    Add files via upload

---
+++ 1 prac.js
@@ -0,0 +1,13 @@
+const mystring= "jiya";
+const myID=38;
+const boolen = true;
+
+function add(num1,num2){
+    return num1+num2;
+}
+
+console.log(myID)
+console.log(boolen)
+console.log(mystring)
+const ans=add(18,14)
+console.log("Sum =",ans)
